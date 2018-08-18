998cb4ad6a21382d8a1301c5b959f4d19cdc2c6d
vaibhav bhawalkar <vaibhavbhawalkar98.vb@gmail.com>
2018-08-18 23:03:54 +0530

    impl SGD

--- Linear Regression/housing-prices.py
+++ Linear Regression/housing-prices.py
@@ -1,9 +1,52 @@
-import sklearn.datasets as skds
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import sklearn.datasets as skds
+import sklearn.preprocessing as pp
+from sklearn.model_selection import train_test_split
+
 dataset = skds.load_boston()
 
-df = pd.DataFrame(dataset.data)
+df = pd.DataFrame(dataset['data'])
 df.columns = dataset['feature_names']
-df['PRICE'] = dataset.target*1000
+X = df.values
+mms = pp.MinMaxScaler()
+X = mms.fit_transform(X)
+# print(pd.DataFrame(X).head(3))
 
-print(df.head())
+from math import floor
+
+
+def LinearRegressor(X, y, inital_weight=0, initial_bias=0, n_batch=43, epochs=100, learning_rate=0.01):
+    N, weight, bias, cost = float(len(y)), inital_weight, initial_bias, None
+    x = X.reshape(n_batch, floor(len(X)/n_batch))
+    for i in range(epochs):
+        for Xi in X:
+            y_pred = (weight * Xi) + bias
+            diff = (y - y_pred)
+            cost = sum([x**2 for x in diff]) / N
+            weight_gradient = (-2 / N) * (sum([x for x in diff*Xi]))
+            bias_gradient = (-2 / N) * (sum([x for x in diff]))
+            weight -= learning_rate * weight_gradient
+            bias -= learning_rate * bias_gradient
+    return weight, bias, cost
+
+
+X_train, x_test, y_train, y_test = train_test_split(
+    X, dataset['target'] * 1000, test_size=0.15)
+
+X_train, x_test = np.array([sum(x) for x in X_train]), np.array([
+    sum(x) for x in x_test])
+
+weight, bias, cost = LinearRegressor(X_train, y_train)
+
+y_pred = weight * x_test + bias
+
+plt.scatter(x_test, y_test)
+plt.plot(x_test, y_pred, color='red')
+plt.xlabel('Actual Price ($)')
+plt.ylabel('Predicted Price ($)')
+plt.show()
+
+# from sklearn.metrics import r2_score
+# print(r2_score(y_test, y_pred))
